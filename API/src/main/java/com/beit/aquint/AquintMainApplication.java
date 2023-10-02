94afee79ae9e1f74190f9457f777911176c3c324
jkalariya <argusadmin>
2023-10-02 15:52:44 +0530

    Intial Commit

---
+++ API/src/main/java/com/beit/aquint/AquintMainApplication.java
@@ -0,0 +1,13 @@
+package com.beit.aquint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquintMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquintMainApplication.class, args);
+	}
+
+}
---
+++ API/src/main/java/com/beit/aquint/auth/models/ERole.java
@@ -0,0 +1,7 @@
+package com.beit.aquint.auth.models;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
+}
