b4c5ed63184203ccc01f2974f160fe3d7af68fef
lithium-hydrogen-arsenic <schoudhary@argusoft.com>
2024-01-18 13:58:18 +0530

    changed baseURL

--- UI-New/src/config/https.js
+++ UI-New/src/config/https.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-  baseURL: "http://65.0.86.93:8080",
+  baseURL: "http://localhost:8080",
   // headers: {
   //   'Content-Type': 'application/json',
   // },
