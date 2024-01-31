4e25db6d2d88dc04d9b21198c4fab7e51abc6802
Patnaik-R <rpatnaik@argusoft.com>
2024-01-31 19:18:27 +0530

    Added dashboard protection

--- UI-New/src/routes.js
+++ UI-New/src/routes.js
@@ -107,6 +107,7 @@ import Division from "layouts/pages/client/Division";
 import Department from "layouts/pages/client/Department";
 import Illustration from "layouts/authentication/sign-in/illustration";
 import Roles from "layouts/pages/roles";
+import Protected from "layouts/authentication/sign-in/Protected";
 
 const routes = [
   {
@@ -115,7 +116,7 @@ const routes = [
     key: "dashboards",
     icon: <Shop size="12px" />,
     collapse: [
-      { name: "Dashboard", key: "Dashboard", route: "/dashboard", component: <CRM /> },
+      { name: "Dashboard", key: "Dashboard", route: "/dashboard", component: <Protected Component={CRM} /> },
       // {
       //   name: "Default",
       //   key: "default",
